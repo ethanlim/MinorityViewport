cb221360b88bb0616866f99f122770671136ff2e
ethanlim <mail@ethanlim.net>
2013-10-08 01:34:43 +0800

    Implemented the server component

--- MultpleKinectsPlatformServer.cpp
+++ MultpleKinectsPlatformServer.cpp
@@ -1,17 +1,40 @@
 #include "MutlipleKinectsPlatformServer.h"
 
-/**
- *  Constructors & Destructors
- */
-MultipleKinectsPlatformServer::Core::Core(){
+namespace MultipleKinectsPlatformServer{
 
-}
+	Core::Core(){
+	  try
+	  {
+		string address = "127.0.0.1"; //localhost
+		string port = "1626";
+		string docRoot = "C:\\Users\\ethanlim\\Documents\\Projects\\School\\MultipleKinectsPlatformServer";
+		MultipleKinectsPlatformServer::JobsQueue *jobQueue;
 
-MultipleKinectsPlatformServer::Core::~Core(){
+		// Initialise the server.
+		jobQueue = new MultipleKinectsPlatformServer::JobsQueue();
+		std::size_t num_threads = boost::lexical_cast<std::size_t>(2);
+
+		http::server::server s(address, port, docRoot, jobQueue, num_threads);
+
+		// Run the server until stopped.
+		s.run();
+	  }
+	  catch (std::exception& e)
+	  {
+		std::cerr << "exception: " << e.what() << "\n";
+	  }
+
+	}
+
+	Core::~Core(){
+
+	}
 
 }
 
 int main( int argc, const char* argv[] )
 {
-  return 0;
+	MultipleKinectsPlatformServer::Core platform;
+
+	return 0;
 }
--- MutlipleKinectsPlatformServer.h
+++ MutlipleKinectsPlatformServer.h
@@ -1,13 +1,20 @@
+
+#ifndef MULTIPLEKINECTSPLATFORMSERVER_H
+#define MULTIPLEKINECTSPLATFORMSERVER_H
+
 #include <iostream>
-#include <ctime>
 #include <string>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/lexical_cast.hpp>
+#include "Networks/Server.h"
+#include "Data/JobsQueue.h"
 
 using namespace std;
 
 namespace MultipleKinectsPlatformServer{
 
 	class Core{
-
 		public :
 			Core();
 			~Core();
@@ -15,3 +22,5 @@ namespace MultipleKinectsPlatformServer{
 	};
 
 }
+
+#endif
