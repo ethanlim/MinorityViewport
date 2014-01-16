2c05405657c3229ea440a14a56f3f7d499786617
ethanlim <mail@ethanlim.net>
2014-01-16 22:11:23 +0800

    Add the Timer that tracks the time of the server

---
+++ Misc/Timer.cpp
@@ -0,0 +1,45 @@
+#include "Timer.h"
+
+namespace MultipleKinectsPlatformServer{
+	
+	Timer::Timer(clock_t startAt){
+		this->_startedAt = startAt;
+		this->_pausedAt = 0;
+		this->_paused = false;
+		this->_started = false;
+	}
+	
+	void Timer::Start()
+	{
+		if( this->_started ){
+			return;
+		}
+
+		this->_started = true;
+		this->_paused = false;
+
+		this->_internal_counter = clock();
+	}
+
+	void Timer::Reset()
+	{
+		this->_paused = false;
+		this->_startedAt = clock();
+	}
+
+	clock_t Timer::GetTicks()
+	{
+		if( !this->_started ){
+			return 0;
+		}
+
+		if( this->_paused ){
+			return this->_pausedAt - this->_startedAt;
+		}
+
+		double seconds_passed = clock() - this->_internal_counter;
+
+		return this->_startedAt+seconds_passed;
+	}
+
+}
---
+++ Misc/Timer.h
@@ -0,0 +1,28 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <ctime>
+
+using namespace std;
+
+namespace MultipleKinectsPlatformServer{
+
+	class Timer
+	{
+		private:
+			clock_t _startedAt;
+			clock_t _pausedAt;
+			clock_t _internal_counter;
+			bool _started;
+			bool _paused;
+		public:
+			Timer::Timer(clock_t startAt);
+	
+			/* Operations */
+			void Start();
+			void Reset();
+			clock_t GetTicks();
+	};
+}
+
+#endif
